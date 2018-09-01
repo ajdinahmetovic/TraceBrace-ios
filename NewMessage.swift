312011f1f8d323143f3a430ba9a83707d4c5bec7
Ajdin Ahmetovic <ajdinahmetovic@networkss-imac-5.local>
2018-09-01 21:05:35 +0200

    Tweak

---
+++ NewMessage.swift
@@ -0,0 +1,17 @@
+//
+//  NewMessage.swift
+//  Trace Brace
+//
+//  Created by Ajdin Ahmetovic on 30/08/2018.
+//  Copyright © 2018 Ajdin Ahmetovic. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NewMessage: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- Trace Brace/Messagess.swift
+++ Trace Brace/Messagess.swift
@@ -11,14 +11,85 @@ import UIKit
 
 class Messagess: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewDidAppear(_ animated: Bool) {
         
         
         
         print("aksndkjaskjnda")
-
         
+        let localDb = UserDefaults.standard
+        
+        let messagess = localDb.array(forKey: "messagess")
+        let phoneNumbers = localDb.array(forKey: "phoneNumbers")
+        
+        
+        
+        if localDb.bool(forKey: "newMessage") {
+            print("newMessages is here")
+            localDb.set(false, forKey: "newMessage")
+            localDb.synchronize()
+            viewDidLoad()
+        }
+        
+        
+        
+        if localDb.object(forKey: "phoneNumbers") != nil {
+            print("Not null")
+            
+            for number in phoneNumbers! {
+                print(number)
+            }
+            for message in messagess! {
+                print(message)
+            }
+            
+        } else {
+            print("isNull")
+        }
+        
+        
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+        /*
+        
+        
+        print("aksndkjaskjnda")
+        
+        let localDb = UserDefaults.standard
+        
+        let messagess = localDb.array(forKey: "messagess")
+        let phoneNumbers = localDb.array(forKey: "phoneNumbers")
+        
+        
+        
+        if localDb.bool(forKey: "newMessage") {
+            print("newMessages is here")
+            localDb.set(false, forKey: "newMessage")
+            localDb.synchronize()
+            viewDidLoad()
+        }
+        
+        
+        
+        if localDb.object(forKey: "phoneNumbers") != nil {
+            print("Not null")
+        
+        for number in phoneNumbers! {
+            print(number)
+        }
+        for message in messagess! {
+            print(message)
+        }
+        
+        } else {
+            print("isNull")
+        }
+ */
         
       }
     
