aac7dac215b639e99186b915ab38d0218e7b709d
Ajdin Ahmetovic <ajdinahmetovic@networkss-imac-5.local>
2018-09-01 21:05:10 +0200

    Tweaking

--- Trace Brace/CreateMessage.swift
+++ Trace Brace/CreateMessage.swift
@@ -7,3 +7,89 @@
 //
 
 import Foundation
+import UIKit
+
+class CreateMessage: UIViewController {
+    
+    
+    @IBOutlet weak var messageText: UITextView!
+    @IBOutlet weak var numberText: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        self.messageText.layer.borderColor = UIColor(red:1.00, green:0.75, blue:0.33, alpha:1.0).cgColor
+        self.messageText.layer.borderWidth = 2.3
+        self.messageText.layer.cornerRadius = 5
+        
+        
+        self.numberText.layer.borderColor = UIColor(red:1.00, green:0.75, blue:0.33, alpha:1.0).cgColor
+        self.numberText.layer.borderWidth = 2.3
+        self.numberText.layer.cornerRadius = 5
+        
+        
+    }
+    
+    
+    @IBAction func close(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    @IBAction func addMessage(_ sender: Any) {
+        
+         let message = messageText.text
+         let phoneNumber = numberText.text
+        
+        let localDb = UserDefaults.standard
+        
+        if localDb.object(forKey: "phoneNumbers") == nil {
+            
+            //  Doesn't exist
+        
+            
+            
+            let phoneNumbers = [phoneNumber]
+            let messagess = [message]
+            
+            localDb.set(phoneNumbers, forKey: "phoneNumbers")
+            localDb.set(messagess, forKey: "messagess")
+            
+            localDb.synchronize()
+            
+            
+        } else {
+            var phoneNumbers = localDb.array(forKey: "phoneNumbers");
+            var messagess = localDb.array(forKey: "messagess")
+            
+            phoneNumbers?.append(phoneNumber!)
+            messagess?.append(message!)
+            
+            
+            localDb.set(phoneNumbers, forKey: "phoneNumbers")
+            localDb.set(messagess, forKey: "messagess")
+            
+            localDb.set(true, forKey: "newMessage")
+            
+            localDb.synchronize()
+            
+            let messagessView: Messagess = Messagess();
+            messagessView.viewDidAppear(false)
+            
+            
+
+        }
+        
+        
+        
+        
+        
+        print(message!, phoneNumber!)
+        
+        dismiss(animated: true, completion: nil)
+        
+    }
+}
+
